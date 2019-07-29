1d6384cd2a69b73656c6d966f99b5bf186c9e35e
Lucas Rojas <lucas.rojas@southworks.com>
2019-07-29 12:36:48 -0300

    Restore Article files to prevent merge conflicts

---
+++ demos/PersonalizerTravelAgencyDemo/PersonalizerTravelAgencyDemo/Repositories/IArticleRepository.cs
@@ -0,0 +1,11 @@
+﻿using PersonalizerTravelAgencyDemo.Models;
+using System.Collections.Generic;
+
+namespace PersonalizerTravelAgencyDemo.Repositories
+{
+    public interface IArticleRepository
+    {
+        IList<Article> GetArticles();
+        Article GetArticle(string id);
+    }
+}
